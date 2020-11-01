8d420baf1b038e1162994cbcbe2c89d028ad537f
Ramunas <ramasit85@gmail.com>
2020-11-01 21:14:29 +0200

    C sharp project CRUD, first migration to db done

---
+++ RamoProjectCRUD/RamoProjectCRUD/Controllers/CategoryController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using RamoProjectCRUD.Data;
+
+namespace RamoProjectCRUD.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        public CategoryController() 
+        {
+
+        }
+
+
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
